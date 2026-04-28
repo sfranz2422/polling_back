0a107d6073f1f7e57c75ac07640c3a7c6ccce0f4
sfranz2422 <stephenfranz22@gmail.com>
2026-04-28 09:26:55 -0400

    first commit

---
+++ src/main/java/org/example/polling_app/PollingAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.polling_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PollingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/polling_app/PollingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.polling_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
