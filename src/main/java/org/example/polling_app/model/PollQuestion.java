a6f90e8f855bdec0a638987ba93ed09dd1663170
sfranz2422 <stephenfranz22@gmail.com>
2026-04-28 13:53:48 -0400

    Added answer options to question model

--- src/main/java/org/example/polling_app/model/PollQuestion.java
+++ src/main/java/org/example/polling_app/model/PollQuestion.java
@@ -16,8 +16,36 @@ public class PollQuestion {
 
     private Long teacherId;
     private String questionText;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private String optionD;
     private LocalDateTime createdAt = LocalDateTime.now();
 
+    public String getOptionA() {
+                return optionA;
+    }
+    public void setOptionA(String optionA) {
+        this.optionA = optionA;
+    }
+    public String getOptionB() {
+        return optionB;
+    }
+    public void setOptionB(String optionB) {
+        this.optionB = optionB;
+    }
+    public String getOptionC() {
+        return optionC;
+    }
+    public void setOptionC(String optionC) {
+        this.optionC = optionC;
+    }
+    public String getOptionD() {
+        return optionD;
+    }
+    public void setOptionD(String optionD) {
+        this.optionD = optionD;
+    }
 
     public LocalDateTime getCreatedAt() {
         return createdAt;
